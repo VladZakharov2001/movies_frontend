36702f2dc1e84b85843795c81d01c148e37ed491
kodep <vladislav.zaharov@kodep.ru>
2021-09-23 16:02:13 +0300

    addFilm

--- src/components/AddFilm/index.tsx
+++ src/components/AddFilm/index.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { Route, Router, Switch } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import { Button } from "@material-ui/core";
+import { WiewFilms } from "./styled";
 const AddFilm: React.FC = () => {
   const URLMOVIES =
     "https://api.themoviedb.org/3/discover/movie?api_key=9ab8bf7eac8227309d7ec67bab0298b3&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
@@ -9,12 +11,35 @@ const AddFilm: React.FC = () => {
   const [films, setFilms] = useState<any[]>([]);
   useEffect(() => {
     axios.get(URLMOVIES).then((res) => {
-      console.log("m", res.data);
+      setFilms([...res.data.results]);
     });
   }, []);
-
+  console.log("bb", films);
   return (
-    <div>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</div>
+    <div>
+      <div>Choose your destiny</div>
+      {console.log("filmwiew", films)}
+      {films.map((film, index) => {
+        return (
+          <div>
+            <span>
+              <WiewFilms>
+                <p>{index}</p>
+                <p>{film.original_title}</p>
+                <img
+                  src={`https://image.tmdb.org/t/p/w200/${film.backdrop_path}`}
+                />
+                <p>Популярность{film.popularity}</p>
+                <p>Дата релиза{film.release_date}</p>
+                <div>
+                  <Button>Save it</Button>
+                </div>
+              </WiewFilms>
+            </span>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 export default AddFilm;
