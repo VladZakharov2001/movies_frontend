9877992414d91c081645be69384dfad8de5935c4
kodep <vladislav.zaharov@kodep.ru>
2021-09-24 17:53:38 +0300

    home2

---
+++ src/components/MainPages/components/RowFavMovies/constants.ts
@@ -0,0 +1,2 @@
+export const URL_MOVIES: string = `https://api.themoviedb.org/3/discover/movie?api_key=`;
+export const URL_POSTERS: string = `https://image.tmdb.org/t/p/w200/`;
--- src/components/MainPages/components/RowFavMovies/styled.tsx
+++ src/components/MainPages/components/RowFavMovies/styled.tsx
@@ -2,5 +2,7 @@ import styled from "styled-components";
 export const CheckFilm = styled.div<{ checkingMark: boolean }>`
   p {
     color: ${(props) => (props.checkingMark ? "grey" : "black")};
+    font-size: 16px;
+    font-family: "Times New Roman", Times, serif;
   }
 `;
